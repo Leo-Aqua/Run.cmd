d9b5c4487fca584ae59a2f5c87cd99381b8715cc
Leo Aqua <88901298+Leo-Aqua@users.noreply.github.com>
2022-08-22 20:20:45 +0200

    Add files via upload

---
+++ Rem.cmd
@@ -0,0 +1,19 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+        goto PROGRAMM
+    ) else (
+        echo ERROR: Run as Admin!
+        pause >nul
+    )
+
+:PROGRAMM
+    reg DELETE HKEY_CLASSES_ROOT\Directory\Background\shell\RunCmd /f
+
+    pause >nul
---
+++ Run.cmd
@@ -0,0 +1,21 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+        goto PROGRAMM
+    ) else (
+        echo ERROR: Run as Admin!
+        pause >nul
+    )
+
+:PROGRAMM
+    reg ADD HKEY_CLASSES_ROOT\Directory\Background\shell\RunCmd /d "Run Cmd in current folder" 
+    reg ADD HKEY_CLASSES_ROOT\Directory\Background\shell\RunCmd /v "Extended" 
+    reg ADD HKEY_CLASSES_ROOT\Directory\Background\shell\RunCmd /v "Icon" /d "C:\Windows\System32\cmd.exe"
+    reg ADD HKEY_CLASSES_ROOT\Directory\Background\shell\RunCmd\command /d "cmd.exe /K"
+    pause >nul
